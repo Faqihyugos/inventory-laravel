36f706f01e8e53a8b861126e3e3258f1b2683df4
faqihyugos <faqihyugos@gmail.com>
2025-12-16 11:41:49 +0700

    feat: add various Blade components for UI, including buttons, cards, modals, forms, and tables

---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,12 @@
+@props(['title' => '', 'type' => 'text', 'name' => '', 'placeholder' => '', 'value' => ''])
+
+<div class="mb-3">
+    <label for="{{ $name }}" class="form-label">{{ $title }}</label>
+    <input type="{{ $type }}" class="form-control @error($name) is-invalid @enderror" name="{{ $name }}"
+        placeholder="{{ $placeholder }}" value="{{ $value }}" {{ $attributes }}>
+    @error($name)
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+    @enderror
+</div>
